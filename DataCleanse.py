42323379c4a3f9381dbdcc1484cd7821845e5c76
Carlos Villagran <cjvillagranmata@gmial.com>
2024-05-09 23:48:28 -0600

    DataCleanse, agregada la data para la base de datos

--- DataCleanse.py
+++ DataCleanse.py
@@ -1,14 +1,45 @@
-
+import pandas as pd
 class DataCleaning:
 
     def __init__(self):
         self = self
 
-    def llenarBD(self, playlists):
-        playlists = ["https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S",
+    def llenarBD(self, sp, playlists):
+        playlistsNames = ["https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S",
                      "https://open.spotify.com/playlist/0sDahzOkMWOmLXfTMf2N4N",
                      "https://open.spotify.com/playlist/5KLKS1zjjeqSe6oRgsdUMb",
                      "https://open.spotify.com/playlist/1k59k1PJIk5ZJ6ppbFuzgS",
                      "https://open.spotify.com/playlist/4W4NXbPfqGKyXQEonUa6Nd",
                      "https://open.spotify.com/playlist/06GeoNaCVRzvplRCEwcmvC",
                      "https://open.spotify.com/playlist/1Z5OsiAWaOWgxGOADmwnRj"]
+        playlistsNames.extend(playlists)
+        cancionesBD = []
+        for i in playlistsNames:
+            dataplaylists = sp.getPlaylist(i)["items"]
+
+            for j in range(len(dataplaylists)):
+
+                if dataplaylists[j]["track"]["name"] not in cancionesBD:
+                    cancionesBD.append(dataplaylists[j]["track"]["name"])
+
+        uriList = []
+        for i in cancionesBD:
+            try:
+                uriList.append(sp.searchTrack(i)["uri"])
+            except IndexError:
+                uriList.append(None)
+
+        features = []
+        songsAndFeatures = {}
+        for i in range(len(uriList)):
+            if uriList[i] is not None:
+                features.append(sp.returnFeatures(uriList[i]))
+                songsAndFeatures[cancionesBD[i]] = features
+            else:
+                features.append(None)
+                songsAndFeatures[cancionesBD[i]] = [{'danceability': pd.NA, 'energy': pd.NA, 'key': pd.NA, 'loudness': pd.NA, 'mode': pd.NA, 'speechiness': pd.NA, 'acousticness': pd.NA, 'instrumentalness': pd.NA, 'liveness': pd.NA, 'valence': pd.NA, 'tempo': pd.NA, 'type': 'audio_features', 'id': 'Not found', 'uri': 'Not found', 'track_href': 'Not found', 'analysis_url': 'Not found', 'duration_ms': pd.NA, 'time_signature': pd.NA}]
+
+
+
+        return songsAndFeatures
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from SpotifyConnection import SpotifyConnection
+from DataCleanse import DataCleaning
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -25,34 +26,34 @@ if __name__ == '__main__':
     #print(datos_cancion["artists"][0]["name"])
     #print(sp.getPlaylist("https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S")["items"][1]["track"])
     #print(len(sp.getPlaylist("https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S")["items"]))
-    cancionBD = []
-    dataPlaylist1 = sp.getPlaylist("https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S")["items"]
-    dataPlaylist2 = sp.getPlaylist("https://open.spotify.com/playlist/0sDahzOkMWOmLXfTMf2N4N")["items"]
-    dataPlaylist3 = sp.getPlaylist("https://open.spotify.com/playlist/5KLKS1zjjeqSe6oRgsdUMb")["items"]
-    dataPlaylist4 = sp.getPlaylist("https://open.spotify.com/playlist/1k59k1PJIk5ZJ6ppbFuzgS")["items"]
-    dataPlaylist5 = sp.getPlaylist("https://open.spotify.com/playlist/4W4NXbPfqGKyXQEonUa6Nd")["items"]
-    dataPlaylist6 = sp.getPlaylist("https://open.spotify.com/playlist/06GeoNaCVRzvplRCEwcmvC")["items"]
-    dataPlaylist7 = sp.getPlaylist("https://open.spotify.com/playlist/1Z5OsiAWaOWgxGOADmwnRj")["items"]
-    for i in range(99):
-        cancionBD.append(dataPlaylist1[i]["track"]["name"])
-    for i in range(99):
-        if dataPlaylist2[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist2[i]["track"]["name"])
-    for i in range(99):
-        if dataPlaylist3[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist3[i]["track"]["name"])
-    for i in range(99):
-        if dataPlaylist4[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist4[i]["track"]["name"])
-    for i in range(99):
-        if dataPlaylist5[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist5[i]["track"]["name"])
-    for i in range(46):
-        if dataPlaylist6[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist6[i]["track"]["name"])
-    for i in range(59):
-        if dataPlaylist7[i]["track"]["name"] not in cancionBD:
-            cancionBD.append(dataPlaylist7[i]["track"]["name"])
+    #cancionBD = []
+    #dataPlaylist1 = sp.getPlaylist("https://open.spotify.com/playlist/1RTENWq73MEx1Pop40ZZ5S")["items"]
+    #dataPlaylist2 = sp.getPlaylist("https://open.spotify.com/playlist/0sDahzOkMWOmLXfTMf2N4N")["items"]
+    #dataPlaylist3 = sp.getPlaylist("https://open.spotify.com/playlist/5KLKS1zjjeqSe6oRgsdUMb")["items"]
+    #dataPlaylist4 = sp.getPlaylist("https://open.spotify.com/playlist/1k59k1PJIk5ZJ6ppbFuzgS")["items"]
+    #dataPlaylist5 = sp.getPlaylist("https://open.spotify.com/playlist/4W4NXbPfqGKyXQEonUa6Nd")["items"]
+    #dataPlaylist6 = sp.getPlaylist("https://open.spotify.com/playlist/06GeoNaCVRzvplRCEwcmvC")["items"]
+    #dataPlaylist7 = sp.getPlaylist("https://open.spotify.com/playlist/1Z5OsiAWaOWgxGOADmwnRj")["items"]
+    #for i in range(99):
+    #    cancionBD.append(dataPlaylist1[i]["track"]["name"])
+    #for i in range(99):
+    #    if dataPlaylist2[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist2[i]["track"]["name"])
+    #for i in range(99):
+    #    if dataPlaylist3[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist3[i]["track"]["name"])
+    #for i in range(99):
+    #    if dataPlaylist4[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist4[i]["track"]["name"])
+    #for i in range(99):
+    #    if dataPlaylist5[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist5[i]["track"]["name"])
+    #for i in range(46):
+    #    if dataPlaylist6[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist6[i]["track"]["name"])
+    #for i in range(59):
+    #    if dataPlaylist7[i]["track"]["name"] not in cancionBD:
+    #        cancionBD.append(dataPlaylist7[i]["track"]["name"])
 
-    print(cancionBD)
-    print(len(cancionBD))
+    #canciones = DataCleaning().llenarBD(sp=sp, playlists=["https://open.spotify.com/playlist/37i9dQZF1DWYN0zdqzbEwl"])
+    #print(canciones)
